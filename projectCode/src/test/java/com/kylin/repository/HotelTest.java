a4f6236b13b31e14915454e709d8545b93648710
kylin1 <18795853969@163.com>
2017-02-22 16:32:59 +0800

    数据层可以单元测试

---
+++ projectCode/src/test/java/com/kylin/repository/HotelTest.java
@@ -0,0 +1,56 @@
+package com.kylin.repository;
+
+import com.kylin.model.Hotel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by kylin on 17/02/2017.
+ * All rights reserved.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/spring/j2ee-context.xml"})
+public class HotelTest {
+
+    @Autowired
+    HotelRepository repository;
+
+    @Test
+    public void testGetOne(){
+        Hotel hotel = repository.findOne(2);
+        System.out.println(hotel.getName());
+
+//        Collection<Approval> approvals = hotel.getApprovals();
+//        System.out.println(approvals.size());
+    }
+
+    @Test
+    public void testInsert(){
+        Hotel hotel = new Hotel();
+        hotel.setAccount("te4st");
+        hotel.setPassword("t4est");
+        hotel.setName("tes4t");
+        hotel.setLocation("test4");
+        hotel.setStatus(1);
+
+        repository.save(hotel);
+    }
+
+    @Test
+    public void testDelete(){
+        Hotel hotel = repository.findOne(1);
+        repository.delete(hotel);
+    }
+
+    @Test
+    public void testUpdate(){
+        Hotel hotel = repository.findOne(1);
+
+        hotel.setName("new new new");
+        repository.save(hotel);
+    }
+
+}
